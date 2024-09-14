78c5d6a5a8d99776024cf29f23f44b143d92b702
pranaaavj <jpvelloor@gmail.com>
2024-09-15 00:36:23 +0530

    Stop tracking all the files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
-  important: true,
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
